8c7ac9b788070db292b8a5f1e54779eb567a1e33
JAlvarezGW19 <jalvarezgw19@gmail.com>
2026-01-02 23:36:08 -0400

    feat: add About App dialog with app information and increment app version.

--- lib/widgets/settings/about_app_dialog.dart
+++ lib/widgets/settings/about_app_dialog.dart
@@ -74,7 +74,7 @@ class AboutAppDialog extends StatelessWidget {
                 showLicensePage(
                   context: context,
                   applicationName: l10n.appTitle,
-                  applicationVersion: "1.0.4",
+                  applicationVersion: "1.0.6",
                   applicationLegalese: "Developed by Juan Álvarez",
                 );
               },
@@ -95,7 +95,7 @@ class AboutAppDialog extends StatelessWidget {
           const SizedBox(height: 4),
           Center(
             child: Text(
-              "${l10n.version} 1.0.4",
+              "${l10n.version} 1.0.6",
               style: const TextStyle(color: AppTheme.textSubtle, fontSize: 10),
             ),
           ),
